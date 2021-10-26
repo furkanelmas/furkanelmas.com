9e4084155c023eb01f9c8ca46a936dcd88dd6138
furkanelmas <elmsfrkn1@gmail.com>
2021-10-26 12:00:16 +0300

    updated

--- js/scripts.js
+++ js/scripts.js
@@ -1,28 +1,28 @@
 /* Description: Custom JS file */
 
 
-(function($) {
-    "use strict"; 
-	
+(function ($) {
+    "use strict";
+
     /* Navbar Scripts */
     // jQuery to collapse the navbar on scroll
-    $(window).on('scroll load', function() {
-		if ($(".navbar").offset().top > 60) {
-			$(".fixed-top").addClass("top-nav-collapse");
-		} else {
-			$(".fixed-top").removeClass("top-nav-collapse");
-		}
+    $(window).on('scroll load', function () {
+        if ($(".navbar").offset().top > 60) {
+            $(".fixed-top").addClass("top-nav-collapse");
+        } else {
+            $(".fixed-top").removeClass("top-nav-collapse");
+        }
     });
-    
-	// jQuery for page scrolling feature - requires jQuery Easing plugin
-	$(function() {
-		$(document).on('click', 'a.page-scroll', function(event) {
-			var $anchor = $(this);
-			$('html, body').stop().animate({
-				scrollTop: $($anchor.attr('href')).offset().top
-			}, 600, 'easeInOutExpo');
-			event.preventDefault();
-		});
+
+    // jQuery for page scrolling feature - requires jQuery Easing plugin
+    $(function () {
+        $(document).on('click', 'a.page-scroll', function (event) {
+            var $anchor = $(this);
+            $('html, body').stop().animate({
+                scrollTop: $($anchor.attr('href')).offset().top
+            }, 600, 'easeInOutExpo');
+            event.preventDefault();
+        });
     });
 
     // offcanvas script from Bootstrap + added element to close menu on click in small viewport
@@ -31,10 +31,10 @@
     })
 
     // hover in desktop mode
-    function toggleDropdown (e) {
+    function toggleDropdown(e) {
         const _d = $(e.target).closest('.dropdown'),
             _m = $('.dropdown-menu', _d);
-        setTimeout(function(){
+        setTimeout(function () {
             const shouldOpen = e.type !== 'click' && _d.is(':hover');
             _m.toggleClass('show', shouldOpen);
             _d.toggleClass('show', shouldOpen);
@@ -42,37 +42,26 @@
         }, e.type === 'mouseleave' ? 300 : 0);
     }
     $('body')
-    .on('mouseenter mouseleave','.dropdown',toggleDropdown)
-    .on('click', '.dropdown-menu a', toggleDropdown);
+        .on('mouseenter mouseleave', '.dropdown', toggleDropdown)
+        .on('click', '.dropdown-menu a', toggleDropdown);
 
 
     /* Move Form Fields Label When User Types */
     // for input and textarea fields
-    $("input, textarea").keyup(function(){
-		if ($(this).val() != '') {
-			$(this).addClass('notEmpty');
-		} else {
-			$(this).removeClass('notEmpty');
-		}
-	});
-	
-
-    /* Back To Top Button */
-    // create the back to top button
-    $('body').prepend('<a href="body" class="back-to-top page-scroll">Back to Top</a>');
-    var amountScrolled = 700;
-    $(window).scroll(function() {
-        if ($(window).scrollTop() > amountScrolled) {
-            $('a.back-to-top').fadeIn('500');
+    $("input, textarea").keyup(function () {
+        if ($(this).val() != '') {
+            $(this).addClass('notEmpty');
         } else {
-            $('a.back-to-top').fadeOut('500');
+            $(this).removeClass('notEmpty');
         }
     });
 
 
-	/* Removes Long Focus On Buttons */
-	$(".button, a, button").mouseup(function() {
-		$(this).blur();
-	});
+
+
+    /* Removes Long Focus On Buttons */
+    $(".button, a, button").mouseup(function () {
+        $(this).blur();
+    });
 
 })(jQuery);
